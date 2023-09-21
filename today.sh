ccfc0a3d20bc1ad508ff80b339f1cbdc38fb6698
XiaoQ <elementary2022@163.com>
2023-09-21 09:23:50 +0800

    elementary

--- today.sh
+++ today.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-
+off=0
+if [ $# -gt 1 ]; then
+    off = $1
+fi
 today="$(date +%Y'W'%W'/'%m'-'%d)"
 mkdir -p "${today}"
 pushd "${today}"
-for x in Lang Math English
-do
+for x in Lang Math English; do
     mkdir -p "${x}"
     pushd "${x}"
     touch README.md
@@ -14,21 +16,18 @@ popd
 wk=${today%%/*}
 md_wk="#### ${wk}"
 mmdd=${today##*/}
-for x in Lang Math English
-do
+for x in Lang Math English; do
     mkdir -p "${x}"
     touch "${x}/README.md"
     readme="${x}/README.md"
     grep -Fxq "${md_wk}" "${readme}"
-    if [[ 1 -eq $(echo $?) ]]
-    then
-        echo >> "${readme}"
-        echo "${md_wk}" >> "${readme}"
+    if [[ 1 -eq $(echo $?) ]]; then
+        echo >>"${readme}"
+        echo "${md_wk}" >>"${readme}"
     fi
     md_today='- ['"${mmdd}"'](../'"${today}"'/'"${x}"'/README.md)'
     grep -Fxq -- "${md_today}" "${readme}"
-    if [[ 1 -eq $(echo $?) ]]
-    then
-        echo "${md_today}" >> "${readme}"
+    if [[ 1 -eq $(echo $?) ]]; then
+        echo "${md_today}" >>"${readme}"
     fi
 done
